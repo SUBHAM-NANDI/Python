5db5484f483ab01eb40da78bc12210b3fbbf35ae
Subham Nandi <subhamnandiagt@gmail.com>
2024-08-10 14:21:14 +0530

    Add files via upload

---
+++ Day1_ Band_name_generator_project.py
@@ -0,0 +1,14 @@
+#Create a greeting for your program.
+print("Hi everyone")
+
+#Ask the user for the city that they grew up
+#in and store it in a variable.
+var_city = input("Here did you grow up? ")
+
+#Ask the user for the name of a pet and
+#store it in a variable.
+var_pet = input("What is your pet name? ")
+
+band_name = "Your brand name could be " + var_pet + var_city
+
+print(band_name)
