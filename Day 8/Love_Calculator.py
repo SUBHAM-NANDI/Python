0ca04fd16c7086883f6b027f1d20f319c14f136a
Subham Nandi <subhamnandiagt@gmail.com>
2024-09-12 17:34:23 +0530

    Add files via upload

---
+++ Day 8/Love_Calculator.py
@@ -0,0 +1,21 @@
+def calculate_love_score(name1, name2):
+    combined_name = name1 + name2
+    lower_name = combined_name.lower()
+
+    t = lower_name.count("t")
+    r = lower_name.count("r")
+    u = lower_name.count("u")
+    e = lower_name.count("e")
+    first_digit = t + r + u + e
+
+    l = lower_name.count("l")
+    o = lower_name.count("o")
+    v = lower_name.count("v")
+    e = lower_name.count("e")
+    second_digit = l + o + v + e
+
+    actual_number = int(str(first_digit) + str(second_digit))
+    print(actual_number)
+
+
+calculate_love_score("Angela Yu", "Jack Bauer")
